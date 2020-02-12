c3e8f5516df0fac389d834a161d0475ab59a4c19
Margaret Wismer <margaretgwismer@gmail.com>
2020-02-12 13:58:04 -0500

    Updates

---
+++ precache-manifest.b149c6ffb4cf220d3524ee3b886f6163.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00d2b7541a6216f8e707627b663ec90",
+    "url": "/react-wyndmoor/index.html"
+  },
+  {
+    "revision": "da35e81c466398b5c502",
+    "url": "/react-wyndmoor/static/css/main.a1610f08.chunk.css"
+  },
+  {
+    "revision": "7e2d62b95c1d6c9490fd",
+    "url": "/react-wyndmoor/static/js/2.70143991.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-wyndmoor/static/js/2.70143991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da35e81c466398b5c502",
+    "url": "/react-wyndmoor/static/js/main.808d10c7.chunk.js"
+  },
+  {
+    "revision": "e580b8663eb15f9b8d73",
+    "url": "/react-wyndmoor/static/js/runtime-main.a3841991.js"
+  },
+  {
+    "revision": "9e39e9ca133070da95188bc170394c8e",
+    "url": "/react-wyndmoor/static/media/chestnut-hill-philadelphia-1.9e39e9ca.jpg"
+  },
+  {
+    "revision": "7bb9586d758f59358b11d21f5d595c00",
+    "url": "/react-wyndmoor/static/media/college-photo_22247.7bb9586d.jpg"
+  },
+  {
+    "revision": "fea01ecec3eca9aad31ff7cda80b0ed3",
+    "url": "/react-wyndmoor/static/media/l_20130610-fairmount-park-1200.fea01ece.jpg"
+  },
+  {
+    "revision": "8b20113846f3a96f074d17180da8c807",
+    "url": "/react-wyndmoor/static/media/nicehouse.8b201138.jpg"
+  }
+]);
